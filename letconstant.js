dfc28fd58f69ef0202c6412767ecda65d270e496
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-01-31 23:13:54 +0600

    let default template

---
+++ letconstant.js
@@ -0,0 +1,22 @@
+const hubby = 'ELias';
+console.log(hubby);
+
+const number = [10, 15];
+number[1] = 88;
+number.push(12);
+const nayok = {
+    name: 'Alamin',
+    phone: 5656
+}
+console.log(number);
+console.log(nayok);
+
+let patientName = 'Rahim Mia';
+patientName = 'Karim ALi';
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+    console.log(i, sum);
+}
+
+console.log(patientName);
---
+++ template.js
@@ -0,0 +1,18 @@
+const firstName = 'Justin';
+const lastName = 'TimberLake';
+const fullName = firstName + ' ' + lastName + ' is a good boy';
+const fullName2 = `${firstName} ${lastName} ${12+85+86} is a good boy.`
+
+console.log(fullName);
+console.log(fullName2);
+
+const multiLine = 'I love you\n'
++ 'I need you\n' 
++ 'I miss you\n';
+console.log(multiLine);
+
+const multiLine2 = `I miss you
+But i don't need you
+Ekhn ar Baily Road e 
+tumake niya ghurte mon chay na`;
+console.log(multiLine2);
