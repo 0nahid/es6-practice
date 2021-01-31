49240d81a39a9d26c3ebabae63386f058428a9b0
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-02-01 01:22:40 +0600

    arrow function use

--- arrow.js
+++ arrow.js
@@ -6,6 +6,7 @@
 // };
 
 // Arrow function =() =>
+
 const doubleIt = num => num * 2;
 const result = doubleIt(5);
 console.log(result);
@@ -13,3 +14,16 @@ console.log(result);
 const add = (x, y) => x + y;
 const addNum = add(2, 5);
 console.log(addNum);
+
+const give5 = () => 5;
+const result2 = give5();
+console.log(result2);
+
+const doMath = (x, y) => {
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    return result;
+}
+const result3 = doMath(7,5);
+console.log(result3);
