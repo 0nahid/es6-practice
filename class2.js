9296e079c320d796cd0fcc6860752643b9303b74
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-02-01 22:15:22 +0600

    class 2

---
+++ class2.js
@@ -0,0 +1,55 @@
+// class Parent {
+//     constructor() {
+//         this.fatherName = 'Schwerznegger'
+//     }
+// }
+// class Child extends Parent {
+//     constructor(name) {
+//         super();
+//         this.name = name;
+//     }
+// }
+// const baby = new Child('Nahid');
+// const baby2 = new Child('Hassan');
+// console.log(baby);
+// console.log(baby2);
+
+// class Parent {
+//     constructor() {
+//         this.fatherFather = 'Koddus';
+//     }
+// };
+// class Child extends Parent {
+//     constructor(name) {
+//         super();
+//         this.name = name;
+//     }
+// };
+
+// class Parent {
+//     constructor() {
+//         this.fatherFather = 'Koddus';
+//     }
+// };
+// class Child extends Parent {
+//     constructor(name) {
+//         super();
+//         this.name = name;
+//     }
+// };
+// const baby = new Child('Abul');
+// console.log(baby);
+
+class Parent {
+    constructor() {
+        this.fatherName = 'Abul Hassan '
+    }
+}
+class Child extends Parent {
+    constructor(name) {
+        super()
+        this.name = name;
+    }
+}
+const baby = new Child('Patal');
+console.log(baby);
